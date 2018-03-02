8ca82d17fad8b7c341832fe4c158a9724959af2a
IsaacShrestha <ishrestha@unomaha.edu>
2018-03-01 22:21:07 -0600

    Started android project for kpcoc hymns

---
+++ app/src/main/java/com/ishrestha/kpcoc_hymn/HymnsFrontend.java
@@ -0,0 +1,13 @@
+package com.ishrestha.kpcoc_hymn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HymnsFrontend extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hymns_frontend);
+    }
+}
